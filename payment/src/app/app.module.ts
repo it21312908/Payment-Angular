716902ac697b7ec1a5b68b0062f02759b226268b
it21312908 <it21312908@my.sliit.lk>
2024-04-29 18:38:07 +0530

    post-payment created

--- payment/src/app/app.module.ts
+++ payment/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostPaymentComponent } from './post-payment/post-payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostPaymentComponent
   ],
   imports: [
     BrowserModule,
